80597ab107919428623a1ef6356bc4d6f8cdfee5
leahchoi <soora913@gmail.com>
2018-07-28 17:21:49 -0700

    checking out to master

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,6 @@
+import React from 'react';
 
+function App(){
+    return <h1>"My first React functional component"</h1>
+}
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber(){
+    var random = Math.floor(Math.random() * 1000) + 1;
+    return random;
+}
+const userObj = {
+    name: "Soora",
+    luckyNumber: luckyNumber(),
+}
+function greeting(userObj){
+    return <div className= "container">
+        <h1>{userObj.name}</h1>
+        <h2 className= "text-muted">Your lucky number is: {userObj.luckyNumber}</h2>
+    </div>
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
+
+import App from './components/app'
